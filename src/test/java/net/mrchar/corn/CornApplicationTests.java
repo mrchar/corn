777f34799c7e2f445a1b579f26997f262521d7b5
mrchar <mrchar@qq.com>
2023-04-25 20:23:31 +0800

    初始化项目

---
+++ src/test/java/net/mrchar/corn/CornApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mrchar.corn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
